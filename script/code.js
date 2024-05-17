7ba02f157e4c7a8964c47aec06612d7853b1efd4
NickiSamantha <nickisabels73@gmail.com>
2024-05-17 09:43:14 +0200

    alldone

--- script/code.js
+++ script/code.js
@@ -2,5 +2,42 @@
 
 //Make a variable with a data of a string and check the type of this variable
 
-let firstName = "Neriah"
-console.log(typeof firstName); 
+// let firstName = "Neriah"
+// console.log(typeof firstName); 
+//Answer : string
+
+//Reassign the value to a number and check the type again. Did it change?
+
+// let firstName = 2
+// console.log(typeof firstName);
+
+//Answer : number
+
+//Go ahead and create a variable with every data type you know of. Double check that you covered all of them by using the typeof check.
+
+//Strings
+let lastName = "Petersen"
+console.log(typeof lastName);
+
+//Numbers
+let height = 1.6
+console.log(typeof height);
+
+//Booleans
+let acceptance = true 
+console.log(typeof acceptance);
+
+//Undefined
+let age = undefined 
+console.log(typeof age);
+
+//Null
+let color ;
+console.log(typeof color)
+
+//Objects 
+let selectedColor = null
+console.log(typeof selectedColor)
+
+
+
